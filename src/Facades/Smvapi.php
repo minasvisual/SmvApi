1e5d6644ec59d6a3295fe09e25a814e603e0d09a
Ulisses Mantovani <mantovaniarts@hotmail.com>
2016-09-05 22:53:52 -0300

    inicialização

---
+++ src/Facades/Smvapi.php
@@ -0,0 +1,14 @@
+<?php namespace Minasvisual\Smvapi\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Smvapi extends Facade {
+
+    /**
+    * Get the registered name of the component.
+    *
+    * @return string
+    */
+    protected static function getFacadeAccessor() { return 'smvapi'; }
+
+}
---
+++ src/Smvapi.php
@@ -0,0 +1,10 @@
+<?php namespace Minasvisual\Smvapi;
+
+class Smvapi {
+
+	// You code here.
+	public function minhaFuncao($vlr)
+	{
+		return $vlr.' Modificado!';
+	}
+}
